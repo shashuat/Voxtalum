2f7c35f7644616558fa96808ffa7331da488be68
Shashwat Sharma <zshashwatz@gmail.com>
2023-08-19 23:44:20 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -29,3 +29,15 @@ def load_database_and_embeddings():
         embedding=embeddings
     )
     return db
+
+def transcribe_audio(audio_file_path, openai_key):
+    openai.api_key = openai_key
+    try:
+        with open(audio_file_path, "rb") as audio_file:
+            response = openai.Audio.transcribe("whisper-1", audio_file)
+        return response["text"]
+    except Exception as e:
+        print(f"Error calling Whisper API: {str(e)}")
+        return None
+    
+    
